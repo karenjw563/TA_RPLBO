8fd5902350188fd575b8ba06f0c971030cae1304
CHRadyatma <93330711+7121christo@users.noreply.github.com>
2023-06-06 01:26:31 +0700

    Delete RentalMobil directory

--- RentalMobil/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.rentalmobil {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.rentalmobil to javafx.fxml;
-    exports com.example.rentalmobil;
-}
